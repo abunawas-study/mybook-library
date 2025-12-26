4ebc15b1c451453a719b7052286141ef3bda2af3
Abusadiq Abunawas Nbanba <abunawasabusadiq@gmail.com>
2025-12-26 23:52:36 +0000

    feat: setup React Router and initial page routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-import { useState } from 'react'
+import { createRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { createBrowserRouter, Router } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
+import Favourites from './pages/Favourites'
+import AI_Assistant from './pages/AI_Assistant'
+import BookDetails from './pages/BookDetails'
 
-
-
+function App() {
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <Home />
+        },
+        {
+            path: '/favourites',
+            element: <Favourites />
+        },
+        {
+            path: '/ai-assistant',
+            element: <AI_Assistant />
+        },
+        {
+            path: '/book/:id',
+            element: <BookDetails />
+        }
+    ])
+    return < RouterProvider router={router}/>
+}
 export default App
--- src/pages/AI_Assistant.jsx
+++ src/pages/AI_Assistant.jsx
@@ -1,1 +1,9 @@
-r
+import React from 'react'
+
+const AI_Assistant = () => {
+  return (
+    <div>AI_Assistant</div>
+  )
+}
+
+export default AI_Assistant
