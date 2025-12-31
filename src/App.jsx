9a64721446ac1f007eb82bb4c3ec23373e8ef2cf
Abusadiq Abunawas Nbanba <abunawasabusadiq@gmail.com>
2025-12-31 23:11:41 +0000

    Install tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { createRef, useState } from 'react'
-import './App.css'
-import { createBrowserRouter, Route, Router, Routes } from 'react-router-dom'
-import { RouterProvider } from 'react-router-dom'
-import Home from './pages/Home'
-import Favourites from './pages/Favourites'
-import AI_Assistant from './pages/AI_Assistant'
-import BookDetails from './pages/BookDetails'
+import { createRef, useState } from "react";
+import "./App.css";
+import { createBrowserRouter, Route, Router, Routes } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Favourites from "./pages/Favourites";
+import AI_Assistant from "./pages/AI_Assistant";
+import BookDetails from "./pages/BookDetails";
 
 function App() {
-    return(
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/favourites" element={<Favourites />} />
-                <Route path="/ai-assistant" element={<AI_Assistant />} />
-                <Route path="/book-details/:id" element={<BookDetails />} />
-            </Routes>
-        </Router>
-    )
+  return (
+    <>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="/ai-assistant" element={<AI_Assistant />} />
+        <Route path="/book-details/:id" element={<BookDetails />} />
+    </Routes>
+    </>
+  );
 }
-export default App
+export default App;
