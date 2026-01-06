a51343430a94252940a8571c744e289e3c607815
Abusadiq Abunawas Nbanba <abunawasabusadiq@gmail.com>
2026-01-06 02:55:09 +0000

    Updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import { createRef, useState } from "react";
-import "./App.css";
-import { createBrowserRouter, Route, Router, Routes } from "react-router-dom";
-import { RouterProvider } from "react-router-dom";
-import Home from "./pages/Home";
-import Favourites from "./pages/Favourites";
-import AI_Assistant from "./pages/AI_Assistant";
-import BookDetails from "./pages/BookDetails";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import BookDetails from './pages/BookDetails';
+import Favorites from './pages/Favorites';
+import AI_Assistant from './pages/AI_Assistant';
+import Navbar from './components/layout/NavBar';
+import { FavoritesProvider } from './context/FavoritesContext';
 
 function App() {
   return (
-    <>
-    <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favourites" element={<Favourites />} />
-        <Route path="/ai-assistant" element={<AI_Assistant />} />
-        <Route path="/book-details/:id" element={<BookDetails />} />
-    </Routes>
-    </>
+    <FavoritesProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/book/:isbn" element={<BookDetails />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/ai-assistant" element={<AI_Assistant />} />
+        </Routes>
+      </Router>
+    </FavoritesProvider>
   );
-}
+};
 export default App;
